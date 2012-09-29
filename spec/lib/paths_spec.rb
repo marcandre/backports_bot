4283da3b6fa25cbbfe73b233ded9a4ebed02de7f
Charles Pence <charles@charlespence.net>
2012-09-28 21:52:44 -0400

    Add tests for ExternalCmds, fix Paths tests.

--- spec/lib/paths_spec.rb
+++ spec/lib/paths_spec.rb
@@ -1,23 +1,27 @@
 require_relative '../../lib/paths'
 
+class PathsTester
+  include Paths
+end
+
 describe 'Paths' do
   describe '.config' do
     it 'includes the config.yml filename' do
-      Paths.config.should include('config.yml')
+      PathsTester.new.config_path.should include('config.yml')
     end
     
     it "doesn't include any tildes" do
-      Paths.config.should_not include('~')
+      PathsTester.new.config_path.should_not include('~')
     end
   end
   
   describe '.data' do
     it 'includes the db.sqlite filename' do
-      Paths.data.should include('db.sqlite')
+      PathsTester.new.data_path.should include('db.sqlite')
     end
     
     it "doesn't include any tildes" do
-      Paths.config.should_not include('~')
+      PathsTester.new.data_path.should_not include('~')
     end
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'rspec/autorun'
+require 'thor'
+require_relative '../lib/configuration'
+
+Dir['./spec/support/**/*.rb'].map { |f| require f }
+
+RSpec.configure do |c|
+  c.before(:each) do
+    # Stub out configuration so it doesn't overwrite whatever the user
+    # actually has.
+    Configuration.stub(:load_config!) { }
+    Configuration.stub(:save_config!) { }
+    
+    # Don't write anything to the console
+    Thor::Shell::Basic.any_instance.stub(:say) { }
+    Thor::Shell::Basic.any_instance.stub(:say_status) { }
+    Thor::Shell::Color.any_instance.stub(:say) { }
+    Thor::Shell::Color.any_instance.stub(:say_status) { }
+  end
+end
+
