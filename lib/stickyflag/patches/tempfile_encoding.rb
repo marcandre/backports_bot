ea170c32efcc059ce978225ff3c596988013e30d
Charles Pence <charles@charlespence.net>
2012-10-30 13:25:27 -0400

    Silly logic mistake.

--- lib/stickyflag/patches/tempfile_encoding.rb
+++ lib/stickyflag/patches/tempfile_encoding.rb
@@ -7,7 +7,7 @@ class Tempfile
     def self.new_with_encoding(params)
       # JRuby's Tempfile constructor got the third hash parameter in JRuby
       # 1.7.0-preview1.
-      if RUBY_VERSION >= "1.9.0" || (RUBY_PLATFORM == 'java' && JRUBY_VERSION >= '1.7.0')
+      if RUBY_VERSION >= "1.9.0" && (RUBY_PLATFORM != 'java' || JRUBY_VERSION >= '1.7.0')
         return Tempfile.new(params, Dir.tmpdir, :encoding => "UTF-8")
       else
         # No coverage on Ruby 1.8, ignore
