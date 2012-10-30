848d9945ac15175b1fcf5acb8f67960e8af37233
Charles Pence <charles@charlespence.net>
2012-10-30 13:21:43 -0400

    Some JRuby versions with RUBY_VERSION >= 1.9 don't follow the Tempfile spec.

--- lib/stickyflag/patches/tempfile_encoding.rb
+++ lib/stickyflag/patches/tempfile_encoding.rb
@@ -5,7 +5,9 @@ class Tempfile
   
   unless Tempfile.respond_to?(:new_with_encoding)
     def self.new_with_encoding(params)
-      if RUBY_VERSION >= "1.9.0"
+      # JRuby's Tempfile constructor got the third hash parameter in JRuby
+      # 1.7.0-preview1.
+      if RUBY_VERSION >= "1.9.0" || (RUBY_PLATFORM == 'java' && JRUBY_VERSION >= '1.7.0')
         return Tempfile.new(params, Dir.tmpdir, :encoding => "UTF-8")
       else
         # No coverage on Ruby 1.8, ignore
