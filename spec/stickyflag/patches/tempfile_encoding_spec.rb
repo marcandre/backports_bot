8de7f4ce343864096f0b5ad27d34a0cd87bc9001
Charles Pence <charles@charlespence.net>
2012-11-09 13:23:44 -0500

    Don't run the tempfile encoding spec on RBX.

--- spec/stickyflag/patches/tempfile_encoding_spec.rb
+++ spec/stickyflag/patches/tempfile_encoding_spec.rb
@@ -11,8 +11,10 @@ describe 'Tempfile' do
       f.unlink
     end
     
-    # JRuby <1.7.0 implements Ruby 1.9 but doesn't follow the spec for Tempfile
-    if RUBY_VERSION >= "1.9.0" && (RUBY_PLATFORM != 'java' || JRUBY_VERSION >= '1.7.0')
+    # JRuby <1.7.0 implements Ruby 1.9 but doesn't follow the spec for Tempfile.
+    # RBX doesn't seem to set external_encoding right, at least on some
+    # versions, so don't run that spec there.
+    if RUBY_VERSION >= "1.9.0" && (RUBY_PLATFORM != 'java' || JRUBY_VERSION >= '1.7.0') && (!defined?(RUBY_ENGINE) || RUBY_ENGINE != 'rbx')
       it 'sets the correct external encoding' do
         f = Tempfile.new_with_encoding 'asdf'
         f.external_encoding.should be
